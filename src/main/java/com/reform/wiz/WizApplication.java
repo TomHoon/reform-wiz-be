7f8bbcfc24a9be01c3bfcd30d6b937d7f0084320
tomhoon <gnsdl9108@naver.com>
2025-06-06 17:36:05 +0900

    first commit

---
+++ src/main/java/com/reform/wiz/WizApplication.java
@@ -0,0 +1,13 @@
+package com.reform.wiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reform/wiz/WizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reform.wiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
