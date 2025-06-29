e70b165656caf98b0dd86725cb82527979796de5
SyKim <iisdf@naver.com>
2025-06-29 20:08:19 +0900

    feat: 결제 API

---
+++ src/main/java/com/reform/wiz/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.reform.wiz.repository;
+
+import com.reform.wiz.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+
+}
